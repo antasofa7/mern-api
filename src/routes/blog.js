127fa4b6bd76496d3347c64596867055ea57aac5
antasofa7 <arfiantasofa@gmail.com>
2021-08-19 21:24:55 +0700

    validate request body add dinamyc error response

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const {body} = require('express-validator');
 
 const router = express.Router();
 
 const blogController = require('../controllers/blog');
 
-router.post('/post', blogController.createBlogPost);
+router.post('/post', [
+    body('title').isLength({min: 5}).withMessage('Input title minimal 5 karakter.'),
+    body('body').isLength({min: 5}).withMessage('Input body minimal 5 karakter.')
+], blogController.createBlogPost);
 
 module.exports = router;
