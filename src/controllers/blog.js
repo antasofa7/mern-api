105e0ef8eda868993fd2099be12c4cb3e3e71b3d
antasofa7 <arfiantasofa@gmail.com>
2021-08-22 01:32:51 +0700

    add pagination on API response

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -44,14 +44,37 @@ exports.createBlogPost = (req, res, next) => {
 }
 
 exports.getAllBlogPost = (req, res, next) => {
+    const currentPage = parseInt(req.query.page) || 1;
+    const perPage = parseInt(req.query.perPage) || 5;
+    let totalItems;
+
     BlogPost.find()
+    .countDocuments()
+    .then(count => {
+        totalItems = count;
+        return BlogPost.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage)
+    })
     .then(result => {
         res.status(200).json({
             message: 'Data blog post berhasil dipanggil',
-            data: result
+            data: result,
+            total_data: totalItems,
+            per_page: perPage,
+            current_page: currentPage
         })
     })
     .catch(err => next(err))
+
+    // BlogPost.find()
+    // .then(result => {
+    //     res.status(200).json({
+    //         message: 'Data blog post berhasil dipanggil',
+    //         data: result
+    //     })
+    // })
+    // .catch(err => next(err))
 }
 
 exports.getAllBlogPostById = (req, res, next) => {
