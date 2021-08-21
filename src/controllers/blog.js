60a97d5a1db7e6e79490b61d16014c35c4ab2ce0
antasofa7 <arfiantasofa@gmail.com>
2021-08-21 23:26:57 +0700

    update & delete blog post

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,4 +1,6 @@
 const { validationResult } = require("express-validator");
+const path = require('path');
+const fs = require('fs');
 const BlogPost = require('../models/blog');
 
 exports.createBlogPost = (req, res, next) => {
@@ -21,7 +23,6 @@ exports.createBlogPost = (req, res, next) => {
     const title = req.body.title;
     const image = req.file.path;
     const body = req.body.body;
-
 
     const Posting = new BlogPost({
         title: title,
@@ -69,4 +70,81 @@ exports.getAllBlogPostById = (req, res, next) => {
         })
     })
     .catch(err => next(err))
+}
+
+exports.updateBlogPost = (req, res, next) => {
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        const err = new Error('Invalid Value');
+        err.errorStatus = 400;
+        err.data = errors.array();
+        throw err;
+    }
+
+    if(!req.file) {
+        const err = new Error('Image harus diupload!');
+        err.status = 422;
+        throw err;
+    }
+
+    const title = req.body.title;
+    const image = req.file.path;
+    const body = req.body.body;
+    const postId = req.params.postId;
+
+    BlogPost.findById(postId)
+    .then(post => {
+        if(!post) {
+            const err = new Error('Blog post tidak ditemukan')
+            err.errorStatus = 404;
+            throw err;
+        }
+
+        post.title = title;
+        post.body = body;
+        post.image = image;
+
+        return post.save();
+    })
+    .then(result => {
+        res.status(200).json({
+            message: 'Update blog post sukses',
+            data: result
+        })
+    })
+    .catch(err => next(err))
+
+}
+
+exports.deleteBlogPost = (req, res, next) => {
+    const postId = req.params.postId;
+
+    BlogPost.findById(postId)
+    .then(post => {
+        if(!post) {
+            const err = new Error('Blog post tidak ditemukan')
+            err.errorStatus = 404;
+            throw err;
+        }
+
+        removeImage(post.image);
+        return BlogPost.findByIdAndRemove(postId);
+    })
+    .then(result => {
+        res.status(200).json({
+            message: 'Hapus blog post berhasil',
+            data: result
+        })
+    })
+    .catch(err => next(err))
+}
+
+const removeImage = (filePath) => {
+    console.log('filepath', filePath);
+    console.log('dirname', __dirname);
+    // /home/antasofa/Documents/Online Course/Programming/Youtube/mern/mern-api/
+    filePath = path.join(__dirname, '../..', filePath);
+    fs.unlink(filePath, err => console.log(err));
 }
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -6,11 +6,15 @@ const router = express.Router();
 const blogController = require('../controllers/blog');
 
 router.post('/post', [
-    body('title').isLength({min: 5}).withMessage('Input title minimal 5 karakter.'),
-    body('body').isLength({min: 5}).withMessage('Input body minimal 5 karakter.')
-], blogController.createBlogPost);
-
+                body('title').isLength({min: 5}).withMessage('Input title minimal 5 karakter.'),
+                body('body').isLength({min: 5}).withMessage('Input body minimal 5 karakter.')
+            ], blogController.createBlogPost);
 router.get('/posts', blogController.getAllBlogPost);
-router.get('/posts/:postId', blogController.getAllBlogPostById)
+router.get('/post/:postId', blogController.getAllBlogPostById);
+router.put('/post/:postId', [
+                body('title').isLength({min: 5}).withMessage('Input title minimal 5 karakter.'),
+                body('body').isLength({min: 5}).withMessage('Input body minimal 5 karakter.')
+            ], blogController.updateBlogPost);
+router.delete('/post/:postId', blogController.deleteBlogPost);
 
 module.exports = router;
